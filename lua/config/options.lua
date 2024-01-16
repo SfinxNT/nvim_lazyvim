7b4a54a3c14f0dc3a0ade2b1ac0771e89a6aa24c
mark pletnev <mark.pletnev@relexsolutions.com>
2024-01-16 02:12:28 +0200

    feat: Initial version

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Disable mouse
+vim.o.mouse = ""
+-- Enable linebreak = ""
+vim.o.breakindent = true
+vim.o.showbreak = "↪ "
+
+-- Set linebreak width (optional)
+vim.o.linebreak = true
+vim.o.showbreak = "↪ "
+vim.o.breakindentopt = "sbr"
+
+-- Set wrap
+vim.wo.wrap = true
---
+++ lua/plugins/core.lua
@@ -0,0 +1,96 @@
+return {
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        dockerls = {},
+        terraformls = {},
+        ansiblels = {
+          filetypes = {
+            "yaml.ansible",
+            "ansible",
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.autoflake)
+      table.insert(opts.sources, nls.builtins.formatting.isort)
+      table.insert(opts.sources, nls.builtins.formatting.black)
+      table.insert(opts.sources, nls.builtins.diagnostics.mypy)
+      table.insert(opts.sources, nls.builtins.diagnostics.ruff)
+    end,
+  },
+
+  -- the opts function can also be used to change the default opts:
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_c, { "filetype", icons_enabled = false })
+    end,
+  },
+
+  -- use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+
+  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" },
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "autoflake",
+        "isort",
+        "black",
+        "mypy",
+        "ruff",
+        "ansible-language-server",
+        "ansible-lint",
+      },
+    },
+  },
+
+  {
+    "pearofducks/ansible-vim",
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        python = { "autoflake", "isort", "black" },
+        yaml = { "yamlfix" },
+        ["yaml.ansible"] = { "yamlfix" },
+        ansible = { "yamlfix" },
+      },
+    },
+  },
+}
