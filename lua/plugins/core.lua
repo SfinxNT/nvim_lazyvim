a0f5d751b36089d688569ab1cc7d08e706f3f3d1
mark pletnev <mark.pletnev@relexsolutions.com>
2024-02-10 22:52:49 +0200

    feat: add NoNeckPain

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -97,6 +97,19 @@ return {
     },
   },
   {
+    "shortcuts/no-neck-pain.nvim",
+    opts = {
+      width = 120,
+      bufferOptionsColors = {
+        blend = 5,
+      },
+    },
+    keys = {
+      -- toggle plugin
+      { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "Toggle NoNeckPain" },
+    },
+  },
+  {
     "folke/zen-mode.nvim",
     dependencies = { "folke/twilight.nvim" },
     event = "VeryLazy",
