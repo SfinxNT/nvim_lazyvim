b285432c6f6f74d6952f41788006c54cb2b342fb
Mark Pletnev <sfinx.ekt@gmail.com>
2024-11-17 23:19:05 +0200

    feat(Telescope): add fuzzy search workaround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>sf",
+  '<cmd>Telescope grep_string search="" only_sort_text=true<cr>',
+  { desc = "Fuzzy search" }
+)
