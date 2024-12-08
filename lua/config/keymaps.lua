a440b6cab41d1e17b0c8ba4a71ee16affeeb2ada
Mark Pletnev <sfinx.ekt@gmail.com>
2024-12-08 07:05:40 +0200

    refactor(keymap): use lua function for grep_string

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sf",
-  '<cmd>Telescope grep_string search="" only_sort_text=true<cr>',
-  { desc = "Fuzzy search" }
-)
+-- vim.keymap.set("n", "<leader>sf", LazyVim.pick("grep_string", { search = "" }), { desc = "Fuzzy search" })
+local mk = require("which-key")
+mk.add({
+  {
+    "<leader>sf",
+    LazyVim.pick("grep_string", { search = "" }),
+    desc = "Fuzzy search",
+    icon = { icon = " ", color = "blue" },
+  },
+})
