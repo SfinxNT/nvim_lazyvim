f7ded35759f9c6c3487213a48777efca3c4c9b47
Mark Pletnev <sfinx.ekt@gmail.com>
2024-12-22 00:42:37 +0200

    feat: use vim.keymap instead of which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,33 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set("n", "<leader>sf", LazyVim.pick("grep_string", { search = "" }), { desc = "Fuzzy search" })
-local mk = require("which-key")
-mk.add({
-  {
-    "<leader>sf",
-    LazyVim.pick("grep_string", { search = "" }),
-    desc = "Fuzzy search",
-    icon = { icon = " ", color = "blue" },
-  },
-  {
-    "-",
-    "<cmd>Oil --float<CR>",
-    desc = "Oil in floating",
-  },
-  {
-    "<leader>ci",
-    group = "Copilot",
-    icon = { icon = " ", color = "blue" },
-  },
-  {
-    "<leader>cio",
-    "<cmd>Copilot disable<CR>",
-    desc = "Disable Copilot",
-  },
-  {
-    "<leader>cii",
-    "<cmd>Copilot enable<CR>",
-    desc = "Enable Copilot",
-  },
-})
+vim.keymap.set("n", "<leader>fd", function()
+  require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
+end, { desc = "Find Files (cwd)" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Oil in floating" })
+vim.keymap.set("n", "<leader>cio", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
+vim.keymap.set("n", "<leader>cii", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
