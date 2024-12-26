2abe6b9a9703e864d9d1e2c1b2166564a8fffc47
Mark Pletnev <sfinx.ekt@gmail.com>
2024-12-26 20:23:23 +0200

    feat(bufferline): config bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+      options = {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = { "▕", "▕" },
+        numbers = "none",
+      },
+    },
+  },
+}
