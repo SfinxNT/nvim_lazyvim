fe94310a83103b511cad7ad53791626df020471f
Mark Pletnev <sfinx.ekt@gmail.com>
2024-12-26 19:20:52 +0200

    feat(oil): move keymap config to plugin section

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,21 @@
 return {
   {
     "stevearc/oil.nvim",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
+    lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      default_file_explorer = true,
+      keymaps = {
+        ["q"] = { "actions.close", mode = "n" },
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-v>"] = { "actions.select", opts = { horizontal = true } },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    keys = {
+      { "-", "<cmd>Oil --float<CR>", desc = "Oil in floating" },
+    },
   },
 }
