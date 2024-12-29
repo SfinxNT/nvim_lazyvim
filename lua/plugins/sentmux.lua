1c11ee17c0334e7b2f20d61893be8fca501fdc8d
Mark Pletnev <sfinx.ekt@gmail.com>
2024-12-30 01:46:36 +0200

    feat(sentmux): replace tome with own plugin

---
+++ lua/plugins/sentmux.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "SfinxNT/sentmux.nvim",
+    lazy = true,
+    keys = {
+      { "<leader>p", "<cmd>SendCommand<CR>", desc = "Send to TMUX" },
+    },
+  },
+}
--- lua/plugins/tome.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "laktak/tome",
-    event = "LazyFile",
-  },
-}
