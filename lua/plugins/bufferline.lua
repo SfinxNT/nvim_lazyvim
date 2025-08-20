e7afaf10738f71abf5a56971ca521dbcea8e7c90
Mark Pletnev <sfinx.ekt@gmail.com>
2025-08-20 17:42:07 +0300

    fix(bufferline): workaround for catpuccin breaking changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,6 +3,12 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     dependencies = "nvim-tree/nvim-web-devicons",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
     opts = {
       options = {
         show_buffer_close_icons = false,
