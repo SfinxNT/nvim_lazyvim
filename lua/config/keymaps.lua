d08ba9872c6308a752429bf8d1af943698e2ce91
Mark Pletnev <sfinx.ekt@gmail.com>
2025-11-28 00:39:07 +0500

    chore(keymaps): remove copilot keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>cio", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
-vim.keymap.set("n", "<leader>cii", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
 vim.keymap.set("n", "<leader>z", function()
   os.execute("tmux split-window -v -l 10 -c " .. vim.fn.expand("%:p:h"))
 end, { desc = "Tmux split window" })
